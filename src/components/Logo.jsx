538b59012e01f2e6045bd6e64e9ee39b95876a41
jhubbsy <justin@jwhubbell.com>
2022-07-29 13:51:26 -0500

    fixed license error with gsap

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,6 +1,6 @@
 import React, {useRef, useEffect} from "react";
 import {gsap} from "gsap-trial";
-import {DrawSVGPlugin} from "gsap-trial/DrawSVGPlugin";
+import {DrawSVGPlugin} from "gsap/DrawSVGPlugin";
 
 
 export default function Logo({setFirstLoadStatus}) {
